98571d9e873b8ae469e540e9faa3d1e031ac0d5a
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-07-06 21:12:20 -0300

    Feat: Adicionando validação nos metodos de atualizar

--- ClinicManager.Application/Validators/Care/CreateCareCommandValidator.cs
+++ ClinicManager.Application/Validators/Care/CreateCareCommandValidator.cs
@@ -1,4 +1,5 @@
 ﻿using ClinicManager.Application.Commands.CreateCare;
+using ClinicManager.Application.Commands.UpdateCare;
 using FluentValidation;
 
 namespace ClinicManager.Application.Validators.Care
@@ -39,5 +40,43 @@ namespace ClinicManager.Application.Validators.Care
 
 
         }
+
+
+        public class UpdateCareCommandValidator : AbstractValidator<UpdateCareCommand>
+        {
+            public UpdateCareCommandValidator()
+            {
+
+                RuleFor(p => p.PatientId)
+                    .NotEmpty()
+                    .WithMessage("O Id do Paciente é obrigatório");
+
+                RuleFor(p => p.ServiceId)
+                    .NotEmpty()
+                    .WithMessage("O Id do Serviço é obrigatório");
+
+                RuleFor(p => p.MedicalId)
+                    .NotEmpty()
+                    .WithMessage("O Id do Médico é obrigatório");
+
+                RuleFor(p => p.Healthinsurance)
+                    .MaximumLength(50)
+                    .WithMessage("O Plano de Saúde deve conter no máximo 50 caracteres");
+
+                RuleFor(p => p.Start)
+                    .NotEmpty()
+                    .WithMessage("A Data de Início é obrigatória");
+
+                RuleFor(p => p.End)
+                    .GreaterThan(p => p.Start)
+                    .WithMessage("A Data de Término deve ser maior que a Data de Início")
+                    .When(p => p.End.HasValue);
+
+                RuleFor(p => p.TypeService)
+                    .NotEmpty()
+                    .WithMessage("O Tipo de Serviço é obrigatório");
+
+
+            }
+        }
     }
-}
